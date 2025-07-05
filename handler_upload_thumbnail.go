daada14980a1785670d6da74f98cf7f30527671d
lordvorath <4kill3@gmail.com>
2025-07-05 09:44:35 +0200

    thumbnail upload base64

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse thumbnail", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read image", err)
+		return
+	}
+
+	vidMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video metadata", err)
+		return
+	}
+
+	if vidMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Wrong user", err)
+		return
+	}
+
+	base64image := base64.StdEncoding.EncodeToString(imageData)
+	//thumbURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64image)
+	vidMetadata.ThumbnailURL = &dataURL
+
+	cfg.db.UpdateVideo(vidMetadata)
+
+	respondWithJSON(w, http.StatusOK, vidMetadata)
 }
